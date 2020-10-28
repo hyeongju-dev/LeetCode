4b1bac9e12fb3aa36066d2ffaa5759224e266b69
hyeongju-dev <xxox71@gmail.com>
2020-10-28 15:08:03 +0900

    solve it using more scalable way

--- FizzBuzz.cpp
+++ FizzBuzz.cpp
@@ -3,24 +3,41 @@
 
 #include <vector>
 #include <string>
+#include <unordered_map>
 using namespace std;
+
+using pis = pair<int, string>;
 
 class Solution {
 public:
-    vector<string> fizzBuzz(int n) {
-      vector<string> ans(n);
-      for (int i = 1, fizz = 1, buzz = 1; i <= n; ++i, ++fizz, ++buzz) {
-        if (fizz == 3 && buzz == 5)
-          fizz = buzz = 0, ans[i-1] = "FizzBuzz";
-        else if(fizz == 3)
-          fizz = 0, ans[i-1] = "Fizz";
-        else if (buzz == 5)
-          buzz = 0, ans[i-1] = "Buzz";
+    string generate(int num, int idx, const int itemsSize, string str, const vector<pis>& items) {
+      if (idx == itemsSize) {
+        if (str.empty())
+          return to_string(num);
         else
-          ans[i-1] = to_string(i);
+          return str;
       }
+
+      pis cand = items[idx];
+      int divisor = cand.first;
+      string word = cand.second;
+
+      if (num % divisor == 0)
+        return generate(num, idx+1, itemsSize, str+word, items);
+      else
+        return generate(num, idx+1, itemsSize, str, items);
+    }
+
+    vector<string> fizzBuzz(int n) {
+      vector<pis> items; items.push_back({3, "Fizz"}); items.push_back({5, "Buzz"});
+      vector<string> ans(n);
+      int itemsSize = items.size();
+
+      for (int num = 1; num <= n; ++num)
+        ans[num-1] = generate(num, 0, itemsSize, "", items);
+
       return ans;
     }
 };
 /* Time complextiy: O(N)
- * Space complexity: O(1) */
+ * Space complexity: O(N*k) k is size of itmes; depth of recursive call */
